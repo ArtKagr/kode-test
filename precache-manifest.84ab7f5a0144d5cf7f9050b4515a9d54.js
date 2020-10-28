f471b174614580296d542b454b78ef8c8b12f9db
kagr <artemiokrasz@yandex.ru>
2020-10-28 19:05:30 +0200

    add gh page

---
+++ precache-manifest.84ab7f5a0144d5cf7f9050b4515a9d54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9195e6d1d03ce0843beef0ac4d95b216",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5e2cdf5264aef893cdb",
+    "url": "/static/css/main.b0919b4d.chunk.css"
+  },
+  {
+    "revision": "fe653970a46405d538bd",
+    "url": "/static/js/2.7e4a9ef0.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.7e4a9ef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e2cdf5264aef893cdb",
+    "url": "/static/js/main.a72447cc.chunk.js"
+  },
+  {
+    "revision": "757d6a42295292a4eebd",
+    "url": "/static/js/runtime-main.020ac6d2.js"
+  },
+  {
+    "revision": "bbfcdf7f6c1c7e830e8c8b63f1fb2192",
+    "url": "/static/media/Group 19.bbfcdf7f.svg"
+  }
+]);
--- src/store/actions/action_2.js
+++
@@ -1,3 +0,0 @@
-const ACTION_2 = "ACTION_2"
-
-export default ACTION_2
